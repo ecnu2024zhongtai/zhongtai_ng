8917239de1f7b4b74da102bcc7416e01fbcf1194
CRUDYYDS <crudyyds@gmail.com>
2024-12-06 15:02:33 +0800

    add gridmap sample

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { GaodeComponent } from './components/gaode.component/gaode.component';
 import { HomeComponent } from './components/home.component/home.component';
 import { DriverlineComponent } from './components/driverline.component/driverline.component';
 import { HeatmapComponent } from './components/heatmap.component/heatmap.component';
+import { GridmapComponent } from './components/gridmap.component/gridmap.component';
 
 export const routes: Routes = [
     { path:'', component: HomeComponent},
     { path:'gaode', component: GaodeComponent},
     { path:'driverline', component: DriverlineComponent},
     { path:'heatmap', component: HeatmapComponent},
+    { path:'gridmap', component: GridmapComponent},
 ];
